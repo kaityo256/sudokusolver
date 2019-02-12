b9b04dc3faf4c269b437e9c679bd73478ed085aa
H. Watanabe <kaityo@users.sourceforge.jp>
2019-02-12 17:25:13 +0900

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@
 
 void hidden_singles_row() {
   // TODO:この入力でhidden_singles_rowが間違える
-  std::string str = "006002507000090000000006430500003600038020700020000000001900008400001050080050000";
-  //Grid g(str);
-  //g.hidden_singles_row();
+  std::string str = "0060025070000900000000064305000036000380207000200000000019"
+                    "00008400001050080050000";
+  // Grid g(str);
+  // g.hidden_singles_row();
   Grid::solve(str);
 }
 
 void hidden_singles_column() {
-  std::string str = "400020090500080200009000000010300060080600007050000900000040700608730000000106500";
+  std::string str = "4000200905000802000090000000103000600806000070500009000000"
+                    "40700608730000000106500";
   Grid g(str);
-  //TODO:
+  // TODO:
 }
 
 int main(int argc, char **argv) {
-  hidden_singles_row();
-  return 0;
+  // hidden_singles_row();
+  // return 0;
   stopwatch::timer<> timer("all");
   timer.start();
   if (argc < 2) {
