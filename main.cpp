48dd8e9ea028ec2ed539c60df3cdb1c91e90421e
H. Watanabe <kaityo@users.sourceforge.jp>
2019-02-13 23:31:26 +0900

    updates sudoku_lib

--- main.cpp
+++ main.cpp
@@ -5,17 +5,7 @@
 void hidden_singles_test() {
   std::string str = "0060025070000900000000064305000036000380207000200000000019"
                     "00008400001050080050000";
-  // Grid::solve(str);
-  Grid g(str);
-  mbit m_row[9] = {};
-  mbit m_column[9] = {};
-  mbit m_box[9] = {};
-  g.get_kill_mask(m_row, m_column, m_box);
-  for (int i = 0; i < 9; i++) {
-    std::cout << (m_row[i] == g.remained_row_mask[i]) << std::endl;
-    std::cout << (m_column[i] == g.remained_column_mask[i]) << std::endl;
-    std::cout << (m_box[i] == g.remained_box_mask[i]) << std::endl;
-  }
+  Grid::solve(str);
 }
 
 int main(int argc, char **argv) {
