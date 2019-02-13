013db4fe72b3453a96f1d16c574b7ad168643047
H. Watanabe <kaityo@users.sourceforge.jp>
2019-02-13 23:23:21 +0900

    kill unit mask完成

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 #include <iostream>
 
 void hidden_singles_test() {
-  std::string str = "006002507000090000000006430500003600038020700020000000001900008400001050080050000";
-  Grid::solve(str);
+  std::string str = "0060025070000900000000064305000036000380207000200000000019"
+                    "00008400001050080050000";
+  // Grid::solve(str);
+  Grid g(str);
+  mbit m_row[9] = {};
+  mbit m_column[9] = {};
+  mbit m_box[9] = {};
+  g.get_kill_mask(m_row, m_column, m_box);
+  for (int i = 0; i < 9; i++) {
+    std::cout << (m_row[i] == g.remained_row_mask[i]) << std::endl;
+    std::cout << (m_column[i] == g.remained_column_mask[i]) << std::endl;
+    std::cout << (m_box[i] == g.remained_box_mask[i]) << std::endl;
+  }
 }
 
 int main(int argc, char **argv) {
+  hidden_singles_test();
   return 0;
   stopwatch::timer<> timer("all");
   timer.start();
